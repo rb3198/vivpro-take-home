7742ef88d59a1ec23941295c84465e3d32f1c44e
rb3198 <ronitbhatia@hotmail.com>
2024-10-17 19:54:33 -0400

    Setup theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { Header } from "./components/header";
+import { withTheme } from "./hocs/withTheme";
+import { ThemedProps } from "./types/interfaces/themed_props";
+import styles from "./app.module.scss";
 
-export default App;
+interface AppProps extends ThemedProps {}
+const App: React.FC<AppProps> = (props) => {
+  const { theme, toggleTheme } = props;
+  return (
+    <div id={styles.root}>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+    </div>
+  );
+};
+
+export default withTheme(App);
